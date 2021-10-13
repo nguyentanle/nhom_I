42baa8b45bf415d1769e91ed52aae8bea0d720d4
Tan Le <19211tt1603@mail.tdc.edu.vn>
2021-10-13 12:38:04 +0700

    Create file cate_xuly.php

---
+++ admin/cate_xuly.php
@@ -0,0 +1,26 @@
+<head>
+<link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<div class="noidung" >
+<table border="1">
+<?php
+require 'posts_connect.php';
+// Up bài viết
+if (isset($_POST['btn_submit'])) {
+    $id_cate = $_POST['id_cate'];
+    $name = $_POST['name'];
+
+        $sql = "INSERT INTO category(id_category, NAME ) VALUES ( '$id_cate', '$name' )";
+        if (mysqli_query($conn, $sql)){
+            header('location: cate_view.php');
+            } else{
+            echo '<script language="javascript">alert("Có lỗi trong quá trình xử lý");</script>';
+        }
+}
+?>
+</table>
+</div>
+
+</body>
+</html>
