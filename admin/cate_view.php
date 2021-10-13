27b212fa2720ecc17a70f06b0b7532f5eccbc17f
nhquyen <nguyenhuuquyen2001@gmail.com>
2021-10-13 20:22:08 +0700

    Update css bootstrap

--- admin/cate_view.php
+++ admin/cate_view.php
@@ -12,7 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cate view</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="./public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../public/css/bootstrap.min.css">
     <link rel="stylesheet" href="./public/css/post.css">
 </head>
 
